7e4d2deb7fcd6fb3ee700e47fc854ea890055e6c
B A C <lashkaresangitanarendra62@gmail.com>
2025-01-08 08:56:02 +0530

    ComingSoon Page added

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const passport = require('passport');
-const DiscordStrategy = require('passport-discord').Strategy;
 require('dotenv').config();
 
 const app = express();
@@ -27,43 +23,10 @@ app.get('/musicDash', (req, res) => {
     res.render('musicStatus',{  });
 });
 
-// app.get('/login', (req, res) => {
-//     res.render('login');
-// });
 
-// app.get('/auth/discord', passport.authenticate('discord'));
-// app.get('/auth/discord/callback', passport.authenticate('discord', {
-//     failureRedirect: '/login'
-// }), (req, res) => {
-//     res.redirect('/admin');
-// });
-
-// app.get('/logout', (req, res) => {
-//     req.logout(() => {
-//         res.redirect('/');
-//     });
-// });
-
-// // Admin page for toggling maintenance mode
-// app.get('/admin', adminOnly, (req, res) => {
-//     res.render('admin', { maintenanceMode, maintenanceEndTime });
-// });
-
-// // Set maintenance end time
-// app.post('/admin/set-maintenance-end', adminOnly, (req, res) => {
-//     const { endTime } = req.body;
-//     maintenanceEndTime = new Date(endTime);
-//     maintenanceMode = true;
-//     io.emit('maintenanceUpdate', { maintenanceMode, maintenanceEndTime });
-//     res.json({ success: true });
-// });
-
-// app.post('/admin/toggle-maintenance', adminOnly, (req, res) => {
-//     maintenanceMode = !maintenanceMode;
-//     if (!maintenanceMode) maintenanceEndTime = null;
-//     io.emit('maintenanceUpdate', { maintenanceMode, maintenanceEndTime });
-//     res.json({ maintenanceMode });
-// });
+app.get('/dashboard', (req, res) => {
+    res.render('comingSoon',{  });
+});
 
 // Start server
 server.listen(PORT, () => {
